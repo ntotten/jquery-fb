58220bed8650ae0f7298399cc569a1202f344c7c
Nathan Totten <ntotten@microsoft.com>
2012-03-14 17:02:49 -0700

    Initial commit

---
+++ fb.js
@@ -0,0 +1,52 @@
+/*!
+ * jQuery Facebook Helper
+ * http://github.com/ntotten/jquery-fb
+ *
+ * Copyright 2012, Nathan Totten
+ * Apache License 2.0
+ * https://github.com/ntotten/jquery-fb/blob/master/LICENSE.txt
+ */
+
+(function( $ ) {
+  $.fn.fb = function(appId) {
+
+  	window.fbInit = window.fbInit || false;
+  	if (window.fbInit) {
+  		 $(document).trigger('fb:initialized');
+  		 FB.XFBML.parse();
+  		 return;
+  	}
+
+  	// Aync Init
+  	window.fbAsyncInit = function() {
+  	  window.fbInit = true;
+  	  $(document).trigger('fb:initializing');
+	  FB.init({
+	    appId      : appId,
+	    status     : true, 
+	    cookie     : true,
+	    xfbml      : true,
+	    oauth      : true,
+	  });
+	  $(document).trigger('fb:initialized');
+  	};
+
+  	// Append fb-root
+  	var fbRoot = 'fb-root';
+  	if (!document.getElementById(fbRoot)) {
+  		var element = document.createElement('div');
+  		element.id = fbRoot;
+  		document.body.appendChild(element);
+  	}
+
+  	// Add Facebook Javascript SDK
+  	var js, id = 'facebook-jssdk'; 
+  	if (!document.getElementById(id)) {
+		js = document.createElement('script'); 
+		js.id = id; 
+		js.async = true;
+		js.src = "//connect.facebook.net/en_US/all.js";
+		document.getElementsByTagName('head')[0].appendChild(js);
+  	}
+  };
+})( jQuery );
