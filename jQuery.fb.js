d4dfd2411618e18502af744d2c8b0ae3295068a4
Nathan Totten <nathan@ntotten.com>
2012-05-07 10:34:43 -0500

    Added check for FB.XFBML.parse() and fixed tab/spacing issues.

--- jQuery.fb.js
+++ jQuery.fb.js
@@ -8,47 +8,49 @@
  */
 
 (function( $ ) {
-  $.fn.fb = function(appId, options) {
+	$.fn.fb = function(appId, options) {
 
-    var settings = $.extend({
-        appId      : appId,
-        status     : true, 
-        cookie     : true,
-        xfbml      : true,
-        oauth      : true
-    }, options);
+		var settings = $.extend({
+				appId      : appId,
+				status     : true, 
+				cookie     : true,
+				xfbml      : true,
+				oauth      : true/
+		}, options);
 
-  	window.fbInit = window.fbInit || false;
-  	if (window.fbInit) {
-  		 $(document).trigger('fb:initialized');
-  		 FB.XFBML.parse();
-  		 return;
-  	}
+		window.fbInit = window.fbInit || false;
+		if (window.fbInit) {
+			$(document).trigger('fb:initialized');
+			if (settings.xfbml) {
+				FB.XFBML.parse();
+			}
+			return;
+		}
 
-  	// Aync Init
-  	window.fbAsyncInit = function() {
-  	  window.fbInit = true;
-  	  $(document).trigger('fb:initializing');
-  	  FB.init(settings);
-	  $(document).trigger('fb:initialized');
-  	};
+		// Aync Init
+		window.fbAsyncInit = function() {
+			window.fbInit = true;
+			$(document).trigger('fb:initializing');
+			FB.init(settings);
+			$(document).trigger('fb:initialized');
+		};
 
-  	// Append fb-root
-  	var fbRoot = 'fb-root';
-  	if (!document.getElementById(fbRoot)) {
-  		var element = document.createElement('div');
-  		element.id = fbRoot;
-  		document.body.appendChild(element);
-  	}
+		// Append fb-root
+		var fbRoot = 'fb-root';
+		if (!document.getElementById(fbRoot)) {
+			var element = document.createElement('div');
+			element.id = fbRoot;
+			document.body.appendChild(element);
+		}
 
-  	// Add Facebook Javascript SDK
-  	var js, id = 'facebook-jssdk'; 
-  	if (!document.getElementById(id)) {
-		js = document.createElement('script'); 
-		js.id = id; 
-		js.async = true;
-		js.src = "//connect.facebook.net/en_US/all.js";
-		document.getElementsByTagName('head')[0].appendChild(js);
-  	}
-  };
+		// Add Facebook Javascript SDK
+		var js, id = 'facebook-jssdk'; 
+		if (!document.getElementById(id)) {
+			js = document.createElement('script'); 
+			js.id = id; 
+			js.async = true;
+			js.src = "//connect.facebook.net/en_US/all.js";
+			document.getElementsByTagName('head')[0].appendChild(js);
+		}
+	};
 })( jQuery );
